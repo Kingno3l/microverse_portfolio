20feaa54df5365258119402e69ac56a2df6eb0ed
kingnoel <kingno3l@icloud.com>
2023-01-04 15:06:51 +0100

    project corrected to figma design precisely

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navlink');
+const span = document.getElementsByClassName('bar')[2];
+
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('show');
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('show');
 });
 
+
+
+span.addEventListener('click', function onClick() {
+    console.log(222);
+    // span.classList.toggle('style="background-color:white;');
+    //span.style.backgroundColor = 'white';
+    //span.style.color = 'white';
+})
+
 document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('show');
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('show');
 }));
